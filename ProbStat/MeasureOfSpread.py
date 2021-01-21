ed90d6eca65079ece717222223b4cdf81f94d5c7
monpdev <35469743+monpdev@users.noreply.github.com>
2021-01-21 23:53:31 +0000

    Add files via upload

---
+++ ProbStat/MeasureOfSpread.py
@@ -0,0 +1,17 @@
+sample1 = houses['Year Built'].sample(50, random_state = 1)
+sample2 = houses['Year Built'].sample(50, random_state = 2)
+
+def standard_deviation(array):
+    reference_point = sum(array) / len(array)
+    
+    distances = []
+    for value in array:
+        squared_distance = (value - reference_point)**2
+        distances.append(squared_distance)
+    
+    variance = sum(distances) / len(distances)
+    
+    return sqrt(variance)
+bigger_spread = 'sample 2'
+st_dev1 = standard_deviation(sample1)
+st_dev2 = standard_deviation(sample2)
