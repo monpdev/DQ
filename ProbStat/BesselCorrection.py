dfba23c86b019982e9c758fabfedd2704ac37954
monpdev <35469743+monpdev@users.noreply.github.com>
2021-01-26 23:44:41 +0000

    Add files via upload

---
+++ ProbStat/BesselCorrection.py
@@ -0,0 +1,51 @@
+from math import sqrt
+def standard_deviation(array):
+    reference_point = sum(array) / len(array)
+    
+    distances = []
+    for value in array:
+        squared_distance = (value - reference_point)**2
+        distances.append(squared_distance)
+    
+    variance = sum(distances) / len(distances)
+    
+    return sqrt(variance)
+
+import matplotlib.pyplot as plt
+st_devs = []
+
+for i in range(5000):
+    sample = houses['SalePrice'].sample(10, random_state = i)
+    st_dev = standard_deviation(sample)
+    st_devs.append(st_dev)
+
+#plt.hist(st_devs)
+#plt.axvline(pop_stdev)  # pop_stdev is pre-saved from the last screen
+
+from math import sqrt
+
+def standard_deviation(array):
+    reference_point = sum(array)/len(array)
+    
+    distances = []
+    for value in array:
+        squared_distance = (value - reference_point)**2
+        distances.append(squared_distance)
+        
+    variance = sum(distances)/(len(distances)-1)
+    
+    return sqrt(variance)
+
+import matplotlib.pyplot as plt
+st_devs = []
+
+for i in range(5000):
+    sample = houses['SalePrice'].sample(10, random_state = i)
+    st_dev = standard_deviation(sample)
+    st_devs.append(st_dev)
+    
+plt.hist(st_devs)
+plt.axvline(pop_stdev)
+
+
+
